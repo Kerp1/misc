4bbadeb79ee0c95e75c8c2049162add697306592
Mattias Nilsen <mattias.m.nilsen@gmail.com>
2016-01-12 22:23:03 +0100

    Add rss.py

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Scripts
 Scripts that are somewhat useful
 
-
+#Rss.py
+A script which polls an rss feed and downloads torrents that match a given regex, keeps track of episode numbers already downloaded to prevent multiple downloads of the same episode. 
---
+++ rss.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+import feedparser
+import re
+import subprocess
+import uuid
+import datetime
+import os
+
+RSS_FEED = ''
+ROOT_DIR = '/home/kerp/rss'
+DOWNLOAD_FILE = ROOT_DIR + '/downloaded'
+REGEX_FILE = ROOT_DIR + '/regex'
+LOG_FILE = ROOT_DIR + '/log.txt'
+TORRENT_WATCH_DIR = ""
+
+
+def torrent_already_downloaded(regex, episode):
+   with open(DOWNLOAD_FILE, 'r') as downloaded_list:
+      for downloaded in downloaded_list:
+         if regex + ' ' + episode + '\n' == downloaded:
+            return True
+            
+   return False
+
+def add_to_downloaded_list(regex, episode):
+   with open(DOWNLOAD_FILE, 'a') as downloaded_list:
+      downloaded_list.write(regex + ' ' + episode + '\n')
+
+def get_regex_list(path, flags):
+   with open(path) as regex_file:
+      regex_list = []
+      for regex_string in regex_file:
+         regex_list.append(re.compile(regex_string.rstrip('\n'), flags))
+
+   return regex_list
+
+def download_torrent(link, to_path):
+   subprocess.Popen(['wget',
+                       link,
+                    '--output-document=' + to_path + str(uuid.uuid4()) + '.torrent',
+                    '--output-file=' + ROOT_DIR + '/wget_log'])   
+
+def parse_rss(feed_url):
+   log_file = open(LOG_FILE, 'a')
+   rss_feed = feedparser.parse(feed_url)
+   regex_list = get_regex_list(REGEX_FILE, re.IGNORECASE)
+   for regex in regex_list:
+      for entry in rss_feed['entries']:
+         title = entry.title.encode('utf-8')
+         match = regex.match(title) 
+         if match:
+            print(title)
+            if not torrent_already_downloaded(regex.pattern, match.group(1)):
+               log_file.write(str(datetime.datetime.now()) + 'Downloading: ' + title + '\n')
+               download_torrent(entry.links[0].href, TORRENT_WATCH_DIR)
+               add_to_downloaded_list(regex.pattern, match.group(1))
+
+   log_file.close()
+
+def setup():
+   open(LOG_FILE, 'w')
+   open(DOWNLOAD_FILE, 'w')
+   open(REGEX_FILE, 'w')
+
+
+if __name__ == "__main__":
+
+   setup()
+
+   parse_rss(RSS_FEED)
